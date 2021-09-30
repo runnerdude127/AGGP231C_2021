995c3f5a79d143e1100f205fb3e262152f39a874
runnerdude127 <77517675+runnerdude127@users.noreply.github.com>
2021-09-30 19:40:41 -0400

    lab 2 almost done

--- Lab Projects/231c_Lab02/Assets/scripts/VirtualCUbe.cs
+++ Lab Projects/231c_Lab02/Assets/scripts/VirtualCUbe.cs
@@ -4,13 +4,76 @@ using UnityEngine;
 
 public class VirtualCUbe : MonoBehaviour
 {
-    public void onSeen()
-    {
-        gameObject.SetActive(true);
-    }
+    private Camera cam;
 
-    public void onLost()
+    private Renderer rend;
+    private Material baseMat;
+    public Material touchMat;
+
+    public bool robotnik = false;
+    AudioSource source;
+    public bool muted;
+    float speed = 1;
+
+    private void Start()
     {
-        gameObject.SetActive(false);
+        cam = Camera.main;
+        rend = GetComponent<MeshRenderer>();
+        baseMat = rend.material;
+
+        source = GetComponent<AudioSource>();
+        source.Play();
+    }
+    private void Update()
+    {
+        source.mute = muted;
+
+        if (Input.GetKey(KeyCode.Mouse0))
+        {
+            Ray ray = cam.ScreenPointToRay(Input.mousePosition);
+            RaycastHit hit;
+            if (Physics.Raycast(ray, out hit, Mathf.Infinity))
+            {
+                //Debug.DrawRay(cam.transform.position, point * hit.distance, Color.yellow, 1);
+                Debug.Log("Did Hit");
+                if (hit.transform.gameObject.tag == "tappable")
+                {
+                    rend.material = touchMat;
+                    if (robotnik == true)
+                    {
+                        muted = false;
+                        transform.Rotate(0, .2f * speed, 0, Space.Self);
+                    }
+                    
+                    Debug.Log("HIT TAP");
+                }
+                else
+                {
+                    rend.material = baseMat;
+                    if (robotnik == true)
+                    {
+                        muted = true;
+                    }
+                }
+            }
+            else
+            {
+               // Debug.DrawRay(cam.transform.position, point * 1000, Color.white, 1);
+                rend.material = baseMat;
+                if (robotnik == true)
+                {
+                    muted = true;
+                }
+                Debug.Log("Did not Hit");
+            }
+        }
+        else
+        {
+            rend.material = baseMat;
+            if (robotnik == true)
+            {
+                muted = true;
+            }
+        }
     }
 }
